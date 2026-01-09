6062f3984185279ef3ef4e0a20a1a587a3b88e1e
williamdennis <william.dennis@auroraer.com>
2026-01-09 10:07:12 +0000

    adding partial functions

--- rect_main.py
+++ rect_main.py
@@ -1,15 +1,20 @@
 from lightweight_screen_capture.display import display
 from lightweight_screen_capture.functions import show_smooth_fps
-from lightweight_screen_capture.screen_capture import capture_window
+from lightweight_screen_capture.screen_capture import capture_window, capture_screen
 from lightweight_screen_capture.rectangle_detector import detect_rectangles
 
 from functools import partial
 
-# detect_rectangles = partial(detect_rectangles, min_percentage=0.05, max_percentage=0.8, show_confidence=False)
+source_function = partial(capture_window, window_name="Task Manager")
+source_function = partial(capture_screen, monitor=2)
+
+detect_rectangles = partial(
+    detect_rectangles,
+    min_percentage=0.01,)
+
 
 if __name__ == "__main__":
     display(
         [show_smooth_fps, detect_rectangles],
-        source=capture_window,
-        source_kwargs={"window_name": "Task Manager"},
+        source=source_function,
     )
